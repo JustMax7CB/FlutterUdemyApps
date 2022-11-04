7bca991f92da1bb9b63eafde1ac182b973e8217d
Max Shapira <83203304+JustMax7CB@users.noreply.github.com>
2022-11-05 00:03:09 +0200

    Update main.dart

--- ibmi/lib/main.dart
+++ ibmi/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
-      title: "IMBI",
+      title: "IBMI",
       routes: {
         '/': (BuildContext context) => MainPage(),
       },
